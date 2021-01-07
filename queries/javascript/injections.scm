53d55ba0473c3ac58e25ce3d016a0409481c645c
Steven Sojka <Steven.Sojka@tdameritrade.com>
2021-01-07 13:52:17 -0600

    fix injected languages

--- queries/javascript/injections.scm
+++ queries/javascript/injections.scm
@@ -11,7 +11,8 @@
                                 (pair
                                   key: ((property_identifier) @_prop
                                         (#eq? @_prop "template"))
-                                  value: ((template_string) @html)))))))
+                                  value: ((template_string) @html
+                                          (#offset! @html 0 1 0 -1))))))))
 
 (export_statement
   decorator: (decorator
@@ -23,7 +24,8 @@
                                 (pair
                                   key: ((property_identifier) @_prop
                                         (#eq? @_prop "template"))
-                                  value: ((template_string) @html))))))
+                                  value: ((template_string) @html
+                                          (#offset! @html 0 1 0 -1)))))))
   declaration: (class_declaration))
 
 (class_declaration
@@ -37,7 +39,8 @@
                                   key: ((property_identifier) @_prop
                                         (#eq? @_prop "styles"))
                                   value: (array
-                                           ((template_string) @css))))))))
+                                           ((template_string) @css
+                                            (#offset! @css 0 1 0 -1)))))))))
 
 (export_statement
   decorator: (decorator
@@ -50,5 +53,6 @@
                                   key: ((property_identifier) @_prop
                                         (#eq? @_prop "styles"))
                                   value: (array
-                                           ((template_string) @css)))))))
+                                           ((template_string) @css
+                                            (#offset! @css 0 1 0 -1))))))))
   declaration: (class_declaration))
